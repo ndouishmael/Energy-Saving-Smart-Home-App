1ad3ddbdff9b0817105194fe75d35cabbddecf8a
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-06-08 12:01:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = 'money_talks'
 
 # user data stored in memory
 users = [
