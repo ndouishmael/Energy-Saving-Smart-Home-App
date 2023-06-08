a116f3d328251efe67c02b6b5f5cc26dc24e6858
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-06-08 20:42:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,47 @@
-from flask import Flask, render_template, request, redirect, session
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for, session
+from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
 app.secret_key = 'money_talks'
 
-# user data stored in memory
-users = [
-    {'username': 'user1', 'password': generate_password_hash('password1')},
-    {'username': 'user2', 'password': generate_password_hash('password2')}
-]
-
+# Routes
 @app.route('/')
 def home():
-    if 'username' in session:
-        return f"Welcome, {session['username']}!"
-    return 'Home page'
+    return render_template('home.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-        user = next((user for user in users if user['username'] == username), None)
-        if user and check_password_hash(user['password'], password):
-            session['username'] = username
-            return redirect('/')
-        else:
-            return 'Invalid username or password'
-    
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Perform login authentication
+        # ...
+        # Set session data
+        session['username'] = form.username.data
+        return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-        if next((user for user in users if user['username'] == username), None):
-            return 'Username already exists'
-        
-        hashed_password = generate_password_hash(password)
-        users.append({'username': username, 'password': hashed_password})
-        
-        return 'Registration successful! Please <a href="/login">login</a>'
-    
-    return render_template('register.html')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Perform user registration
+        # ...
+        # Set session data
+        session['username'] = form.username.data
+        return redirect(url_for('dashboard'))
+    return render_template('register.html', form=form)
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return f"Welcome, {session['username']}!"
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    return redirect('/')
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
