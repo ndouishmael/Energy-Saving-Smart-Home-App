a4deee59fd7a200002fd1cb700be64264decb43a
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-06-08 12:59:06 +0200

    Create dashboard.js

---
+++ js/dashboard.js
@@ -0,0 +1,12 @@
+// JavaScript code for user dashboard functionality
+// You can add any desired functionality here
+
+// Example: Display a welcome message to the user
+var user = {
+  name: "John Doe",
+  email: "john@example.com",
+  role: "User"
+};
+
+var welcomeMessage = "Welcome, " + user.name + "! You are logged in as a " + user.role;
+console.log(welcomeMessage);
