89e1b425b90ac429cd52b6e670a60085ce6eff02
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-06-08 21:29:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from forms import LoginForm, RegistrationForm
+from flask import Flask, render_template, session, redirect, url_for, request
 
 app = Flask(__name__)
 app.secret_key = 'money_talks'
 
-# Routes
 @app.route('/')
 def home():
-    return render_template('home.html')
+    if 'username' in session:
+        return "Welcome, {}!".format(session['username'])
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Perform login authentication
-        # ...
-        # Set session data
-        session['username'] = form.username.data
-        return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        # Perform user registration
-        # ...
-        # Set session data
-        session['username'] = form.username.data
-        return redirect(url_for('dashboard'))
-    return render_template('register.html', form=form)
-
-@app.route('/dashboard')
-def dashboard():
-    if 'username' in session:
-        return "Welcome, " + session['username'] + "!"
-    else:
-        return redirect(url_for('login'))
+    if request.method == 'POST':
+        username = request.form['username']
+        session['username'] = username
+        return redirect(url_for('home'))
+    return render_template('login.html')
 
 @app.route('/logout')
 def logout():
